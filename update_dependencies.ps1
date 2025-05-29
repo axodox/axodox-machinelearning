41cfb3ebcf368f7249aaae528cc0a2e06fdad1ef
Peter Major <peter.major@cae.com>
2025-05-29 19:25:51 +0200

    Better dependency update

--- update_dependencies.ps1
+++ update_dependencies.ps1
@@ -1,12 +1,18 @@
 $dependencies = @("Axodox.Common")
 
-$pluginProjects = Resolve-Path "./*/*.vcxproj"
+$projects = Resolve-Path "./*/*.vcxproj"
+$nuspecs = Resolve-Path "*.nuspec"
 
 foreach ($dependency in $dependencies) {
-  $latestPackage = .\Tools\nuget.exe list -allversions $dependency
+  Write-Host "Fetching version of $dependency..."
+
+  $latestPackage = .\Tools\nuget.exe list -allversions $dependency | Where-Object { $_ -match "^$dependency\s+" }
   $latestVersion = [regex]::Match($latestPackage, "\d+(\.\d+)+").Value
 
-  foreach ($projectPath in $pluginProjects) {
+  Write-Host "  Found: $latestVersion"
+
+  # Update projects
+  foreach ($projectPath in $projects) {
     # Update project file
     $projectContent = Get-Content -Path $projectPath -Raw
     $projectContent = $projectContent -replace "\\packages\\$dependency\.([^\\]+)\\", ("\packages\$dependency.$1" + $latestVersion + '\')
@@ -14,11 +20,24 @@ foreach ($dependency in $dependencies) {
 
     # Update packages.config
     $configPath = Join-Path (Split-Path $projectPath) 'packages.config'
-    $configContent = [xml](Get-Content -Path $configPath)
+    $configContent = [xml](Get-Content -Path $configPath -Encoding UTF8)
     $dependencyNodes = $configContent.SelectNodes("//package[contains(@id, '$dependency')]")
     foreach ($dependencyNode in $dependencyNodes) {
       $dependencyNode.version = $latestVersion
     }
     $configContent.Save($configPath)
+  }
+
+  #Update nuspecs
+  foreach ($nuspec in $nuspecs) {
+    $nuspecContent = [xml]::new()
+    $nuspecContent.PreserveWhitespace = $true
+    $nuspecContent.Load($nuspec)
+    
+    $dependencyNodes = $nuspecContent.package.metadata.dependencies.SelectNodes("//*[contains(@id, '$dependency')]")
+    foreach ($dependencyNode in $dependencyNodes) {
+      $dependencyNode.version = $latestVersion
+    }
+    $nuspecContent.Save($nuspec)
   }
 }
